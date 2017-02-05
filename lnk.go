abb305af09b65b895cdbe420b433f61def15e166
reujab <reujab@gmail.com>
2017-02-05 14:47:21 -0500

    parse LinkFlags

--- lnk.go
+++ lnk.go
@@ -8,8 +8,34 @@ import (
 
 // LNK represents the parsed information in a .lnk file.
 type LNK struct {
-	HeaderSize int32
-	CLSID      [16]byte
+	HeaderSize                  int32
+	CLSID                       [16]byte
+	LinkFlags                   int32
+	HasTargetIDList             bool
+	HasLinkInfo                 bool
+	HasName                     bool
+	HasRelativePath             bool
+	HasWorkingDir               bool
+	HasArguments                bool
+	HasIconLocation             bool
+	IsUnicode                   bool
+	ForceNoLinkInfo             bool
+	HasExpString                bool
+	RunInSeperateProcess        bool
+	HasDarwinID                 bool
+	RunAsUser                   bool
+	HasExpIcon                  bool
+	NoPidlAlias                 bool
+	RunWithShimLayer            bool
+	ForceNoLinkTrack            bool
+	EnableTargetMetadata        bool
+	DisableLinkPathTracking     bool
+	DisableKnownFolderTracking  bool
+	DisableKnownFolderAlias     bool
+	AllowLinkToLink             bool
+	UnaliasOnSave               bool
+	PreferEnvironmentPath       bool
+	KeepLocalIDListForUNCTarget bool
 }
 
 // ErrInvalidHeaderSize is returned when the header size is not 76.
@@ -46,6 +72,40 @@ func Parse(file io.Reader) (lnk *LNK, err error) {
 	if lnk.CLSID != validCLSID {
 		return lnk, ErrInvalidCLSID
 	}
+
+	err = binary.Read(file, endianness, &lnk.LinkFlags)
+
+	if err != nil {
+		return
+	}
+
+	lnk.HasTargetIDList = lnk.LinkFlags&0x00000001 != 0
+	lnk.HasLinkInfo = lnk.LinkFlags&0x00000002 != 0
+	lnk.HasName = lnk.LinkFlags&0x00000004 != 0
+	lnk.HasRelativePath = lnk.LinkFlags&0x00000008 != 0
+	lnk.HasWorkingDir = lnk.LinkFlags&0x00000010 != 0
+	lnk.HasArguments = lnk.LinkFlags&0x00000020 != 0
+	lnk.HasIconLocation = lnk.LinkFlags&0x00000040 != 0
+	lnk.IsUnicode = lnk.LinkFlags&0x00000080 != 0
+	lnk.ForceNoLinkInfo = lnk.LinkFlags&0x00000100 != 0
+	lnk.HasExpString = lnk.LinkFlags&0x00000200 != 0
+	lnk.RunInSeperateProcess = lnk.LinkFlags&0x00000400 != 0
+	// Unused1
+	lnk.HasDarwinID = lnk.LinkFlags&0x00001000 != 0
+	lnk.RunAsUser = lnk.LinkFlags&0x00002000 != 0
+	lnk.HasExpIcon = lnk.LinkFlags&0x00004000 != 0
+	lnk.NoPidlAlias = lnk.LinkFlags&0x00008000 != 0
+	// Unused2
+	lnk.RunWithShimLayer = lnk.LinkFlags&0x00020000 != 0
+	lnk.ForceNoLinkTrack = lnk.LinkFlags&0x00040000 != 0
+	lnk.EnableTargetMetadata = lnk.LinkFlags&0x00080000 != 0
+	lnk.DisableLinkPathTracking = lnk.LinkFlags&0x00100000 != 0
+	lnk.DisableKnownFolderTracking = lnk.LinkFlags&0x00200000 != 0
+	lnk.DisableKnownFolderAlias = lnk.LinkFlags&0x00400000 != 0
+	lnk.AllowLinkToLink = lnk.LinkFlags&0x00800000 != 0
+	lnk.UnaliasOnSave = lnk.LinkFlags&0x01000000 != 0
+	lnk.PreferEnvironmentPath = lnk.LinkFlags&0x02000000 != 0
+	lnk.KeepLocalIDListForUNCTarget = lnk.LinkFlags&0x04000000 != 0
 
 	return
 }
