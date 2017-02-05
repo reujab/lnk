f41f7439c914307d2ec3b333ddcae176316841e1
reujab <reujab@gmail.com>
2017-02-05 15:18:30 -0500

    parse FileAttributes

--- lnk.go
+++ lnk.go
@@ -8,8 +8,9 @@ import (
 
 // LNK represents the parsed information in a .lnk file.
 type LNK struct {
-	HeaderSize                  int32
-	CLSID                       [16]byte
+	HeaderSize int32
+	CLSID      [16]byte
+	// For more information, view https://msdn.microsoft.com/en-us/library/dd891314.aspx or https://github.com/libyal/liblnk/blob/15ec0a6ea940e79048ceee71861546485c4ab6d8/documentation/Windows%20Shortcut%20File%20%28LNK%29%20format.asciidoc#21-data-flags.
 	LinkFlags                   int32
 	HasTargetIDList             bool
 	HasLinkInfo                 bool
@@ -36,6 +37,25 @@ type LNK struct {
 	UnaliasOnSave               bool
 	PreferEnvironmentPath       bool
 	KeepLocalIDListForUNCTarget bool
+	FileAttributes              int32
+	// For more information, view https://msdn.microsoft.com/en-us/library/dd871338.aspx or https://github.com/libyal/liblnk/blob/15ec0a6ea940e79048ceee71861546485c4ab6d8/documentation/Windows%20Shortcut%20File%20%28LNK%29%20format.asciidoc#file_attribute_flags.
+	FileAttribute struct {
+		ReadOnly          bool
+		Hidden            bool
+		System            bool
+		Reserved1         bool
+		Directory         bool
+		Archive           bool
+		Reserved2         bool
+		Normal            bool
+		Temporary         bool
+		SparseFile        bool
+		ReparsePoint      bool
+		Compressed        bool
+		Offline           bool
+		NotContentIndexed bool
+		Encrypted         bool
+	}
 }
 
 // ErrInvalidHeaderSize is returned when the header size is not 76.
@@ -43,6 +63,9 @@ var ErrInvalidHeaderSize = errors.New("invalid header size")
 
 // ErrInvalidCLSID is returned when the CLSID is not valid
 var ErrInvalidCLSID = errors.New("invalid CLSID")
+
+// ErrReservedBitSet is returned when a reserved bit is set
+var ErrReservedBitSet = errors.New("reserved bit set")
 
 var endianness = binary.LittleEndian
 var validCLSID = [...]byte{
@@ -106,6 +129,32 @@ func Parse(file io.Reader) (lnk *LNK, err error) {
 	lnk.UnaliasOnSave = lnk.LinkFlags&0x01000000 != 0
 	lnk.PreferEnvironmentPath = lnk.LinkFlags&0x02000000 != 0
 	lnk.KeepLocalIDListForUNCTarget = lnk.LinkFlags&0x04000000 != 0
+
+	err = binary.Read(file, endianness, &lnk.FileAttributes)
+
+	if err != nil {
+		return
+	}
+
+	lnk.FileAttribute.ReadOnly = lnk.FileAttributes&0x00000001 != 0
+	lnk.FileAttribute.Hidden = lnk.FileAttributes&0x00000002 != 0
+	lnk.FileAttribute.System = lnk.FileAttributes&0x00000004 != 0
+	lnk.FileAttribute.Reserved1 = lnk.FileAttributes&0x00000008 != 0
+	lnk.FileAttribute.Directory = lnk.FileAttributes&0x00000010 != 0
+	lnk.FileAttribute.Archive = lnk.FileAttributes&0x00000020 != 0
+	lnk.FileAttribute.Reserved2 = lnk.FileAttributes&0x00000040 != 0
+	lnk.FileAttribute.Normal = lnk.FileAttributes&0x00000080 != 0
+	lnk.FileAttribute.Temporary = lnk.FileAttributes&0x00000100 != 0
+	lnk.FileAttribute.SparseFile = lnk.FileAttributes&0x00000200 != 0
+	lnk.FileAttribute.ReparsePoint = lnk.FileAttributes&0x00000400 != 0
+	lnk.FileAttribute.Compressed = lnk.FileAttributes&0x00000800 != 0
+	lnk.FileAttribute.Offline = lnk.FileAttributes&0x00001000 != 0
+	lnk.FileAttribute.NotContentIndexed = lnk.FileAttributes&0x00002000 != 0
+	lnk.FileAttribute.Encrypted = lnk.FileAttributes&0x00004000 != 0
+
+	if lnk.FileAttribute.Reserved1 || lnk.FileAttribute.Reserved2 {
+		return lnk, ErrReservedBitSet
+	}
 
 	return
 }
