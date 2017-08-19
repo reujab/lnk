80c23900ca33b20293a138cc9fea0e7ea40d5ed7
reujab <reujab@gmail.com>
2017-08-18 20:04:43 -0400

    formatted and refactored

--- lnk.go
+++ lnk.go
@@ -9,23 +9,26 @@ import (
 	"time"
 )
 
-// ShowNormal is the value of LNK.ShowCommand when the application should be
-// opened normally.
-const ShowNormal = 1
+const (
+	// ShowNormal is the value of LNK.ShowCommand when the application should be
+	// opened normally.
+	ShowNormal = 1
 
-// ShowMaximized is the value of LNK.ShowCommand when the application should be
-// opened maximized.
-const ShowMaximized = 3
+	// ShowMaximized is the value of LNK.ShowCommand when the application should be
+	// opened maximized.
+	ShowMaximized = 3
 
-// ShowMinNoActive is the value of LNK.ShowCommand when the application should
-// be opened minimized.
-const ShowMinNoActive = 7
+	// ShowMinNoActive is the value of LNK.ShowCommand when the application should
+	// be opened minimized.
+	ShowMinNoActive = 7
+)
 
 // LNK represents the parsed information in a .lnk file.
 type LNK struct {
 	HeaderSize uint32
 	CLSID      [16]byte
-	// For more information, view https://msdn.microsoft.com/en-us/library/dd891314.aspx or https://github.com/libyal/liblnk/blob/15ec0a6ea940e79048ceee71861546485c4ab6d8/documentation/Windows%20Shortcut%20File%20%28LNK%29%20format.asciidoc#21-data-flags.
+	// https://msdn.microsoft.com/en-us/library/dd891314.aspx
+	// https://github.com/libyal/liblnk/blob/15ec0a6ea940e79048ceee71861546485c4ab6d8/documentation/Windows%20Shortcut%20File%20%28LNK%29%20format.asciidoc#21-data-flags.
 	LinkFlags                   uint32
 	HasTargetIDList             bool
 	HasLinkInfo                 bool
@@ -53,7 +56,8 @@ type LNK struct {
 	PreferEnvironmentPath       bool
 	KeepLocalIDListForUNCTarget bool
 	FileAttributes              uint32
-	// For more information, view https://msdn.microsoft.com/en-us/library/dd871338.aspx or https://github.com/libyal/liblnk/blob/15ec0a6ea940e79048ceee71861546485c4ab6d8/documentation/Windows%20Shortcut%20File%20%28LNK%29%20format.asciidoc#file_attribute_flags.
+	// https://msdn.microsoft.com/en-us/library/dd871338.aspx
+	// https://github.com/libyal/liblnk/blob/15ec0a6ea940e79048ceee71861546485c4ab6d8/documentation/Windows%20Shortcut%20File%20%28LNK%29%20format.asciidoc#file_attribute_flags.
 	FileAttribute struct {
 		ReadOnly          bool
 		Hidden            bool
@@ -71,13 +75,10 @@ type LNK struct {
 		NotContentIndexed bool
 		Encrypted         bool
 	}
-	CreationTimeNano uint64
-	CreationTime     time.Time
-	AccessTimeNano   uint64
-	AccessTime       time.Time
-	WriteTimeNano    uint64
-	WriteTime        time.Time
-	FileSize         uint32
+	CreationTime time.Time
+	AccessTime   time.Time
+	WriteTime    time.Time
+	FileSize     uint32
 	// documentation says this should be signed, but I can't test this so it will
 	// say unsigned
 	IconIndex uint32
@@ -96,7 +97,7 @@ type LNK struct {
 	Reserved2                              uint32
 	Reserved3                              uint32
 	IDListSize                             uint16
-	IDList                                 []byte
+	IDListBytes                            []byte
 	LinkInfoSize                           uint32
 	LinkInfoHeaderSize                     uint32
 	LinkInfoFlags                          uint32
@@ -117,53 +118,52 @@ type LNK struct {
 	LocalBasePath                          string
 }
 
-// ErrInvalidHeaderSize is returned when the header size is not 76.
-var ErrInvalidHeaderSize = errors.New("invalid header size")
+var (
+	// ErrInvalidHeaderSize is returned when the header size is not 76.
+	ErrInvalidHeaderSize = errors.New("invalid header size")
 
-// ErrInvalidCLSID is returned when the CLSID is not valid
-var ErrInvalidCLSID = errors.New("invalid CLSID")
+	// ErrInvalidCLSID is returned when the CLSID is not valid
+	ErrInvalidCLSID = errors.New("invalid CLSID")
 
-// ErrReservedBitSet is returned when a reserved bit is set
-var ErrReservedBitSet = errors.New("reserved bit set")
+	// ErrReservedBitSet is returned when a reserved bit is set
+	ErrReservedBitSet = errors.New("reserved bit set")
 
-// ErrInvalidHotKey is returned when the hotkey low byte is invalid
-var ErrInvalidHotKey = errors.New("invalid hotkey")
+	// ErrInvalidHotKey is returned when the hotkey low byte is invalid
+	ErrInvalidHotKey = errors.New("invalid hotkey")
+)
 
 var endianness = binary.LittleEndian
+
 var validCLSID = [...]byte{
 	1, 20, 2, 0, 0, 0, 0, 0,
 	192, 0, 0, 0, 0, 0, 0, 70,
 }
 
 // Parse parses an io.Reader into a LNK.
-func Parse(file io.Reader) (lnk *LNK, err error) {
-	lnk = new(LNK)
-	err = binary.Read(file, endianness, &lnk.HeaderSize)
+func Parse(file io.Reader) (*LNK, error) {
+	reader := bufio.NewReader(file)
+	lnk := new(LNK)
 
+	err := binary.Read(file, endianness, &lnk.HeaderSize)
 	if err != nil {
-		return
+		return lnk, err
 	}
-
 	if lnk.HeaderSize != 76 {
 		return lnk, ErrInvalidHeaderSize
 	}
 
 	_, err = file.Read(lnk.CLSID[:])
-
 	if err != nil {
-		return
+		return lnk, err
 	}
-
 	if lnk.CLSID != validCLSID {
 		return lnk, ErrInvalidCLSID
 	}
 
 	err = binary.Read(file, endianness, &lnk.LinkFlags)
-
 	if err != nil {
-		return
+		return lnk, err
 	}
-
 	lnk.HasTargetIDList = lnk.LinkFlags&0x00000001 != 0
 	lnk.HasLinkInfo = lnk.LinkFlags&0x00000002 != 0
 	lnk.HasName = lnk.LinkFlags&0x00000004 != 0
@@ -193,11 +193,9 @@ func Parse(file io.Reader) (lnk *LNK, err error) {
 	lnk.KeepLocalIDListForUNCTarget = lnk.LinkFlags&0x04000000 != 0
 
 	err = binary.Read(file, endianness, &lnk.FileAttributes)
-
 	if err != nil {
-		return
+		return lnk, err
 	}
-
 	lnk.FileAttribute.ReadOnly = lnk.FileAttributes&0x00000001 != 0
 	lnk.FileAttribute.Hidden = lnk.FileAttributes&0x00000002 != 0
 	lnk.FileAttribute.System = lnk.FileAttributes&0x00000004 != 0
@@ -213,66 +211,59 @@ func Parse(file io.Reader) (lnk *LNK, err error) {
 	lnk.FileAttribute.Offline = lnk.FileAttributes&0x00001000 != 0
 	lnk.FileAttribute.NotContentIndexed = lnk.FileAttributes&0x00002000 != 0
 	lnk.FileAttribute.Encrypted = lnk.FileAttributes&0x00004000 != 0
-
 	if lnk.FileAttribute.Reserved1 || lnk.FileAttribute.Reserved2 {
 		return lnk, ErrReservedBitSet
 	}
 
-	err = binary.Read(file, endianness, &lnk.CreationTimeNano)
-
+	var creationTime uint64
+	err = binary.Read(file, endianness, &creationTime)
 	if err != nil {
-		return
+		return lnk, err
 	}
+	lnk.CreationTime = windowsNanoToTime(creationTime)
 
-	lnk.CreationTime = windowsNanoToTime(lnk.CreationTimeNano)
-	err = binary.Read(file, endianness, &lnk.AccessTimeNano)
-
+	var accessTime uint64
+	err = binary.Read(file, endianness, &accessTime)
 	if err != nil {
-		return
+		return lnk, err
 	}
+	lnk.AccessTime = windowsNanoToTime(accessTime)
 
-	lnk.AccessTime = windowsNanoToTime(lnk.AccessTimeNano)
-	err = binary.Read(file, endianness, &lnk.WriteTimeNano)
-
+	var writeTime uint64
+	err = binary.Read(file, endianness, &writeTime)
 	if err != nil {
-		return
+		return lnk, err
 	}
+	lnk.WriteTime = windowsNanoToTime(writeTime)
 
-	lnk.WriteTime = windowsNanoToTime(lnk.WriteTimeNano)
 	err = binary.Read(file, endianness, &lnk.FileSize)
-
 	if err != nil {
-		return
+		return lnk, err
 	}
 
 	err = binary.Read(file, endianness, &lnk.IconIndex)
-
 	if err != nil {
-		return
+		return lnk, err
 	}
 
 	err = binary.Read(file, endianness, &lnk.ShowCommand)
-
 	if err != nil {
-		return
+		return lnk, err
 	}
 
 	err = binary.Read(file, endianness, &lnk.HotKeyLowByte)
-
 	if err != nil {
-		return
+		return lnk, err
 	}
 
 	err = binary.Read(file, endianness, &lnk.HotKeyHighByte)
-
 	if err != nil {
-		return
+		return lnk, err
 	}
 
 	if lnk.HotKeyLowByte < 0x30 || (lnk.HotKeyLowByte > 0x39 && lnk.HotKeyLowByte < 0x41) || (lnk.HotKeyLowByte > 0x5a && lnk.HotKeyLowByte < 0x70) || (lnk.HotKeyLowByte > 0x87 && lnk.HotKeyLowByte < 0x90) || lnk.HotKeyLowByte > 0x91 {
 		return lnk, ErrInvalidHotKey
 	}
-
 	if lnk.HotKeyLowByte >= 0x70 && lnk.HotKeyLowByte <= 0x87 {
 		lnk.HotKey.Key = "F" + strconv.Itoa(int(lnk.HotKeyLowByte-0x6f))
 	} else if lnk.HotKeyLowByte == 0x90 {
@@ -282,27 +273,23 @@ func Parse(file io.Reader) (lnk *LNK, err error) {
 	} else {
 		lnk.HotKey.Key = string(lnk.HotKeyLowByte)
 	}
-
 	lnk.HotKey.Shift = lnk.HotKeyHighByte&1 != 0
 	lnk.HotKey.Ctrl = lnk.HotKeyHighByte&2 != 0
 	lnk.HotKey.Alt = lnk.HotKeyHighByte&4 != 0
 
 	err = binary.Read(file, endianness, &lnk.Reserved1)
-
 	if err != nil {
-		return
+		return lnk, err
 	}
 
 	err = binary.Read(file, endianness, &lnk.Reserved2)
-
 	if err != nil {
-		return
+		return lnk, err
 	}
 
 	err = binary.Read(file, endianness, &lnk.Reserved3)
-
 	if err != nil {
-		return
+		return lnk, err
 	}
 
 	if lnk.Reserved1 != 0 || lnk.Reserved2 != 0 || lnk.Reserved3 != 0 {
@@ -311,131 +298,109 @@ func Parse(file io.Reader) (lnk *LNK, err error) {
 
 	if lnk.HasTargetIDList {
 		err = binary.Read(file, endianness, &lnk.IDListSize)
-
 		if err != nil {
-			return
+			return lnk, err
 		}
-
-		lnk.IDList = make([]byte, lnk.IDListSize)
-		_, err = file.Read(lnk.IDList)
-
+		lnk.IDListBytes = make([]byte, lnk.IDListSize)
+		_, err = file.Read(lnk.IDListBytes)
 		if err != nil {
-			return
+			return lnk, err
 		}
 	}
 
 	if lnk.HasLinkInfo {
 		err = binary.Read(file, endianness, &lnk.LinkInfoSize)
-
 		if err != nil {
-			return
+			return lnk, err
 		}
 
 		err = binary.Read(file, endianness, &lnk.LinkInfoHeaderSize)
-
 		if err != nil {
-			return
+			return lnk, err
 		}
 
 		err = binary.Read(file, endianness, &lnk.LinkInfoFlags)
-
 		if err != nil {
-			return
+			return lnk, err
 		}
-
 		lnk.VolumeIDAndLocalBasePath = lnk.LinkInfoFlags&1 != 0
 		lnk.CommonNetworkRelativeLinkAndPathSuffix = lnk.LinkInfoFlags&2 != 0
 
 		err = binary.Read(file, endianness, &lnk.VolumeIDOffset)
-
 		if err != nil {
-			return
+			return lnk, err
 		}
 
 		err = binary.Read(file, endianness, &lnk.LocalBasePathOffset)
-
 		if err != nil {
-			return
+			return lnk, err
 		}
 
 		err = binary.Read(file, endianness, &lnk.CommonNetworkRelativeLinkOffset)
-
 		if err != nil {
-			return
+			return lnk, err
 		}
 
 		err = binary.Read(file, endianness, &lnk.CommonPathSuffixOffset)
-
 		if err != nil {
-			return
+			return lnk, err
 		}
 
 		if lnk.LinkInfoHeaderSize > 28 {
 			err = binary.Read(file, endianness, &lnk.LocalBasePathOffsetUnicode)
-
 			if err != nil {
-				return
+				return lnk, err
 			}
 		}
 
 		if lnk.LinkInfoHeaderSize > 32 {
 			err = binary.Read(file, endianness, &lnk.CommonPathSuffixOffsetUnicode)
-
 			if err != nil {
-				return
+				return lnk, err
 			}
 		}
 
 		if lnk.VolumeIDAndLocalBasePath {
 			err = binary.Read(file, endianness, &lnk.VolumeIDSize)
-
 			if err != nil {
-				return
+				return lnk, err
 			}
 
 			err = binary.Read(file, endianness, &lnk.DriveType)
-
 			if err != nil {
-				return
+				return lnk, err
 			}
 
 			err = binary.Read(file, endianness, &lnk.DriveSerialNumber)
-
 			if err != nil {
-				return
+				return lnk, err
 			}
 
 			err = binary.Read(file, endianness, &lnk.VolumeLabelOffset)
-
 			if err != nil {
-				return
+				return lnk, err
 			}
 
 			if lnk.VolumeLabelOffset > 16 {
 				err = binary.Read(file, endianness, &lnk.VolumeLabelOffsetUnicode)
-
 				if err != nil {
-					return
+					return lnk, err
 				}
 			}
 
-			reader := bufio.NewReader(file)
-
 			lnk.VolumeLabel, err = reader.ReadString('\x00')
-
 			if err != nil {
-				return
+				return lnk, err
 			}
 
 			lnk.LocalBasePath, err = reader.ReadString('\x00')
-
 			if err != nil {
-				return
+				return lnk, err
 			}
 		}
 	}
 
-	return
+	return lnk, nil
 }
 
 // The Windows epoch is 1601-01-01, while the Unix epoch is 1970-01-01.
@@ -443,7 +408,6 @@ func windowsNanoToTime(windowsNano uint64) time.Time {
 	// fmt.Println(time.Unix((windowsNano-116444736000000000)/10000000, 0))
 	// fmt.Println(time.Unix(0, 1000000000*((windowsNano-116444736000000000)/10000000)))
 	// fmt.Println(time.Unix(0, 100*windowsNano-11644473600000000000))
-	// most accurate method
 	// fmt.Println(time.Unix(0, 100*(windowsNano-116444736000000000)))
 
 	// this converts the Windows nanoseconds to Unix nanoseconds
